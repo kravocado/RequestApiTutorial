f297d7d0ca9fa46e7c70ecc386e93366991656f3
SeoDongyeon <abocado@seodong-yeon-ui-MacBookPro.local>
2021-10-14 16:12:55 +0900

    Use Api make SearchCollectionView

---
+++ RequestApiTutorial/ImageCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  ImageCollectionViewCell.swift
+//  RequestApiTutorial
+//
+//  Created by SeoDongyeon on 2021/10/08.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ImageCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    func configure(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else { return }
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                self?.imageView.image = image
+            }
+        }.resume()
+    }
+}
